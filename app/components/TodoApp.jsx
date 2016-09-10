e24fb35445abf60b9768d7d0db28f15b8d7f0433
silent-sea1119 <sandragonzales56t@gmail.com>
2016-09-10 16:26:33 -0400

    Add filter functionality

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -50,12 +50,13 @@ var TodoApp = React.createClass({
     });
   },
   render: function () {
-    var {todos} = this.state;
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos} onToggle={this.handleToggle}/>
+        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     )
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -55,4 +55,51 @@ describe('TodoAPI', () => {
       expect(actualTodos).toEqual(todos);
     });
   });
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id:1,
+      text: 'some text',
+      completed:true
+    }, {
+      id:2,
+      text: 'some text',
+      completed:false
+    }, {
+      id:3,
+      text: 'text',
+      completed:true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      var filterTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filterTodos.length).toBe(3);
+    });
+
+    it('should return non-completed todos when showCompleted is false', () => {
+      var filterTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filterTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', () => {
+      var filterTodos = TodoAPI.filterTodos(todos, true, 'some');
+
+      expect(filterTodos.length).toBe(2);
+    });
+
+    it('should return all todos if searchText is empty', () => {
+      var filterTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filterTodos.length).toBe(3);
+    });
+
+  });
 });
